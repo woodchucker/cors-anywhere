0e3b7b085bbd19430bd636e7dc149f1c281c80b8
woodchucker <ditrafabri@gmail.com>
2022-12-14 12:20:12 +0100

    remove header to ensure is not required

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ var port = process.env.PORT || 8080;
 var cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
     originWhitelist: [], // Allow all origins
-    requireHeader: ['origin', 'x-requested-with'],
+    requireHeader: [
+        // 'origin', 'x-requested-with'
+    ],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, function() {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
